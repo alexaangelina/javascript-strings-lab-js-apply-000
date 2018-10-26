b0c5cc05486e9e53511459ae41eba9c333cf0ff4
Alexa Harmon <alexa.angelina7@gmail.com>
2018-10-26 22:50:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var specialGuest = "Neil deGrasse Tyson";
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 `Hello, ${specialGuest}! High ${3 + 2}!`; 
 // "Hello, Neil deGrasse Tyson!";
-var topic = "space";
+var topic = "Let\'s talk about space";
 var conversation = `${topic}`;
 // "Let's talk about space";
